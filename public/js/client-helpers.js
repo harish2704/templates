f24acbd55b707f9e6a92b33ebe5061cd324ba7c5
Harish.K <harish@harilocal.in>
2014-03-10 14:44:31 +0530

    use browserify

---
+++ public/js/client-helpers.js
@@ -0,0 +1,13 @@
+var helpers = {};
+var lingo = require('lingo');
+helpers.camelcase= lingo.camelcase;
+helpers.small = function( str ){
+    return str.toLowerCase();
+};
+helpers.capitalize = lingo.capitalize;
+helpers.singularize = function ( str ) {
+    return lingo.en.singularize( str );
+};
+helpers.pluralize = function(str){ return lingo.en.pluralize(str); };
+
+module.exports = helpers;
