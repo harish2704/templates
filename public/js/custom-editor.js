6f7592c2a5f41d4f2d1124666e5f19f138924266
Harish.K <harish@harilocal.in>
2014-03-15 13:19:50 +0530

    Added common snippets for editing Ect templates
    Signed-off-by: Harish.K <harish@harilocal.in>

--- public/js/custom-editor.js
+++ public/js/custom-editor.js
@@ -1,5 +1,47 @@
 var SearchBox = ace.acequire('ace/ext/searchbox').SearchBox;
 ace.acequire('ace/ext/searchbox');
+var snippetManager = ace.acequire("ace/snippets").snippetManager;
+
+var ectSnippets = "snippet %\n\
+	<% ${1} %>\n\
+snippet =\n\
+	<%= @${1:escaped} %>\n\
+snippet -\n\
+	<%- @${1:unescaped} %>\n\
+snippet content\n\
+	<%content '${1}'%>${2}\n\
+snippet block\n\
+	<% block '${1}' : %>\n\
+		${2}\n\
+	<% end %>${3}\n\
+snippet extend\n\
+	<% extend '${1:layout.html}' %>\n\
+snippet %if\n\
+	<% if @${1}?${2:.length} : %>\n\
+		${3}\n\
+	<% else : %>\n\
+		${4}\n\
+	<% end %>\n\
+snippet %switch\n\
+	<% switch @${1} : %>\n\
+		<% when '${2}' : %>\n\
+			${3}\n\
+		<% end %>\n\
+		<% else : %>\n\
+			${4}\n\
+		<% end %>\n\
+	<% end %>${5}\n\
+snippet %when\n\
+	<% when '${1}' : %>\n\
+		${2}\n\
+	<% end %>${3}\n\
+snippet %for\n\
+	<% for ${1:item} in @${2:$1s} : %>\n\
+		${3}\n\
+	<% end %>";
+
+
+
 
 var customEditorOptions = {
     enableBasicAutocompletion: true,
@@ -15,5 +57,11 @@ var createAceEditor = function( elem ){
     editor.setOptions( customEditorOptions );
     var sb = new SearchBox( editor );
     sb.hide();
+    var mode = editor.getSession().getMode().$id;
+    var snip = snippetManager.files[mode];
+    var snippets = snippetManager.parseSnippetFile( ectSnippets, snip.mode );
+    snippetManager.register( snippets );
+
     return editor;
 }
+
